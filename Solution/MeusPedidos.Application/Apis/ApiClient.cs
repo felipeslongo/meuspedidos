a2524f54dfdc7e1610497d5ee2a1ff6fcc38a0d9
Felipe de Souza Longo <felipeslongo@gmail.com>
2018-07-23 21:54:37 -0300

    Criadas os Clients/Consumers das APIS de JSON de Categoria, Produto e Promoção.

---
+++ Solution/MeusPedidos.Application/Apis/ApiClient.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace MeusPedidos.Application.Apis
+{
+    /// <summary>
+    /// Classe base para os clientes de API externa
+    /// </summary>
+    internal abstract class ApiClient
+    {
+        public static Dictionary<string, string> cache = new Dictionary<string, string>();
+
+        public T FetchJson<T>(Uri uri)
+        {
+            if (cache.ContainsKey(uri.AbsoluteUri))
+                return JsonConvert.DeserializeObject<T>(cache[uri.AbsoluteUri]);
+
+            var json = new System.Net.WebClient().DownloadString(uri);
+            cache[uri.AbsoluteUri] = json;
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
